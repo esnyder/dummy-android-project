0eb6ce82bdcd90e16a5153f0d17cc0a74415cd3a
Emile Snyder <emile@circleci.com>
2015-01-02 17:18:11 -0800

    update sys install to x86

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -7,7 +7,7 @@ DEPS="$ANDROID_HOME/installed-dependencies"
 if [ ! -e $DEPS ]; then
     cp -r /usr/local/android-sdk-linux $ANRDROID_HOME &&
     echo y | android update sdk --no-ui --all --filter "android-21, build-tools-21.1.2"
-    echo y | android update sdk --no-ui --all --filter "sys-img-armeabi-v7a-android-21"
+    echo y | android update sdk --no-ui --all --filter "sys-img-x86-android-21"
     #echo y | android update sdk --no-ui --all --filter "extra-android-m2repository"
     #echo y | android update sdk --no-ui --all --filter "extra-google-m2repository"
     #echo y | android update sdk --no-ui --all --filter "extra-android-support"
